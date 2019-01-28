b0312b513473c9402974b34d2e165117e235bfa6
Shubham Rai <shubham010793@gmail.com>
2019-01-28 07:46:36 +0530

    Updated the version of java to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,13 +1,25 @@
 package clinic.programming.training;
 
+import java.util.*;
+
 public class Application {
 	public Application() {
 		System.out.println("Inside application");
+	}
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+
+		for (String greet : greetings) {
+			System.out.println("Greeting: " + greet);
+		}
 	}
 	
 	//	method main(): Always the application entry point
 	public static void main(String[] args) {
 		System.out.println("Starting Application");
 		Application app = new Application();
+
+		app.greet();
 	}
 }
